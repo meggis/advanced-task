bb18bb7fb97b079cfbe12c1f3c8f78298d754526
Magdalena Siekańska <magdalena.siekanska@gmail.com>
2021-11-09 18:48:02 +0100

    prepare boilerplate, create table form & fetch contacts with axios

---
+++ src/components/Table.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <!-- <pre>
+            {{ character }}
+    </pre> -->
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Phone number</th>
+          <th scope="col">Email</th>
+          <th scope="col">Country</th>
+          <th scope="col">City</th>
+          <th scope="col">Address</th>
+        </tr>
+      </thead>
+      <tbody v-for="character in characters" :key="character.id">
+        <tr>
+          <td>{{character.name}}</td>
+          <td>{{character.last_name}}</td>
+          <td>{{character.phone_number}}</td>
+          <td>{{character.email}}</td>
+          <td>{{character.country}}</td>
+          <td>{{character.city}}</td>
+          <td>{{character.address}}</td>
+          <td>
+            <button @click="removeItem(character)">Remove</button>
+            <button @click="modifyItem(character)">Modify</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["characters"]),
+  },
+  async mounted() {
+    await this.$store.dispatch("getAllCharacters");
+  },
+  methods: {
+    async removeItem({ id }) {
+      await this.$store.dispatch("removeItem", id);
+    },
+  },
+  async modifyItem({ id }) {
+    console.log(id)
+  }
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    characters: [],
   },
   mutations: {
+    setCharacters(state, newCharacters) {
+      state.characters = newCharacters
+    }
   },
   actions: {
+    async getAllCharacters({ commit }) {
+      await axios
+      .get('http://test01.varid.pl:4080/api/contacts')
+      .then((response) => {
+        commit('setCharacters', response.data)
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    },
+    async removeItem({dispatch}, id) {
+      // console.log(commit)
+      await axios.delete(`http://test01.varid.pl:4080/api/contact/delete/${id}`)
+      dispatch('getAllCharacters')
+    },
   },
   modules: {
   }
